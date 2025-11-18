0e8d20144a91774a6a94ce58a9f1290c4c26f2a4
Siddhesh Thorat <siddhesh.s.thorat14@gmail.com>
2025-11-18 16:19:29 +0530

    this is first push

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Products(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.FloatField(max_digits=10,decimal_places=2)
+    image=models.ImageField(upload_to='Products/')
+    catagory=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
