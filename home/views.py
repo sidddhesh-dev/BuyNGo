074aa22320ec8ea8e0bcbe2c7eaca3b6e5ecba5a
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-03-13 23:30:44 +0530

    navbar modification

--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home/home.html')
+
+def developer(request):
+    return render(request,'home/developer.html')
