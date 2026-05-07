6999e66402138c772a51197f14ea60d735ca2f4f
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-07 19:31:43 +0530

    user register and user login views created

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from . import views
 
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView
+from .views import views
 from . import views
 
 urlpatterns = [
     path("register/", views.user_register,name='register'),
     path("login/", views.user_login,name='login'),
-
     path('accounts/', views.accounts, name='accounts')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
 
 
 def user_register(request):
@@ -9,13 +11,36 @@ def user_register(request):
         password=request.POSt.get("password")
 
         if User.objects.filter(email=email).exists():
+            messages.warning(request,"this email alredy exsists try to login")
             return redirect('register')
+        if User.objects.filter(username=username).exists():
+            messages.warning(request,"username alerady exists")
+            return redirect('register')
+        
+        User.objects.create_user(
+            email=email,
+            username=username,
+            password=password
+        )
+        messages.success(request,"user successfully registred")
+        return redirect('login')
     return render(request,'account/register.html')
 
 def user_login(request):
     if request.method=="POST":
         username=request.POST.get("username")
         password=request.POSt.get("password")
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,"user login successful")
+            return redirect('home')
+
+    
+
+    
 
 
     return render(request,'account/accounts.html')
