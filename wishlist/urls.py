b6d36164152ee5a2152c0ad513dffcddc793e33f
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-10 00:21:18 +0530

    the new products has been added to our project

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
+    path('add_to_wishlist/<int:id>/',views.add_to_wishlist,name='add_to_wishlist')
     
 ]
