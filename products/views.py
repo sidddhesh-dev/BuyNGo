0386895c75d1cf966bbd5a038ade125bfc97c066
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-15 14:55:43 +0530

    checkout view created to render the basic detail and pruchase detial

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Products
 from .api_services import fetch_data
+from django.contrib.auth.decorators import login_required
 
 
 def product_list(request):
@@ -21,7 +22,7 @@ def product_list(request):
 #     products = Products.objects.all()
 #     return render(request, 'products/products.html', {"products": products})
 
-
+@login_required
 def product_details(request, id):
     product = get_object_or_404(Products, id=id)
     return render(request, 'products/product_details.html', {"product": product})
@@ -41,7 +42,9 @@ def search_products(request):
 
     return JsonResponse([], safe=False)
 
+
+@login_required
 def buy_now(request, id):
     product = get_object_or_404(Products, id=id)
     context = {"product": product}
-    return render(request, "products/checkout.html", context)
+    return render(request, "orders/checkout.html", context)
