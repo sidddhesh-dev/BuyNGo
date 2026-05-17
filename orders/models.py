c43976c4cfc3de256ac6da36bf5fed0443a7f039
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-17 12:29:31 +0530

    address model and order model crated

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Address(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='addresses'
+    )
+    full_name=models.CharField(max_length=150)
+    phone=models.CharField(max_length=15)
+    address_line1=models.CharField(max_length=250)
+    address_line2=models.CharField(null=True,blank=True,max_length=250)
+    city=models.CharField(max_length=100)
+    postal_code=models.CharField(max_length=10)
+    state=models.CharField(max_length=20)
+    country=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.full_name
+    
+
+class Order(models.Model):
+
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Pending'
+    )
+
+    payment_method = models.CharField(max_length=50)
+
+    is_paid = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+      
+    
+
+
