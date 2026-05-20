b9d9142943c4c3cbc1947d91364d16a01cbac0b1
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-20 11:56:14 +0530

    Configured static files

--- BuyNGo/settings.py
+++ BuyNGo/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-&al%k20rtcx5n^p6x^jfdw0^d4n2vgw4pr$7p)*@6)3q-#6!9_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'siddhesh2003.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -121,11 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
