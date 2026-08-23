ee67da9ebca844c2ff8647d9a095183b1c8215f5
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-08-23 22:25:29 +0530

    refactor : basic settings.py modification

--- BuyNGo/settings.py
+++ BuyNGo/settings.py
@@ -12,17 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&al%k20rtcx5n^p6x^jfdw0^d4n2vgw4pr$7p)*@6)3q-#6!9_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -30,7 +26,6 @@ ALLOWED_HOSTS = [
 ]
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
